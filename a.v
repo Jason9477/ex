e36784e6c1320276c4d77099da4cfabc4aad66fc
Jason Hsieh <jasonhsieh@Jasons-MacBook-Pro.local>
2026-04-26 15:20:16 +0800

    0426

--- a.v
+++ a.v
@@ -7,25 +7,32 @@ output  out_valid;
 input   [15:0]  b_in;
 output  [31:0]  x_out;
 //==================================================================//IO
-reg signed [31:0] b_in_reg[0:15];
+reg signed [15:0] b_in_reg[0:15];
 reg [15:0]counter;
-
-wire signed [32:0] time6_in1,time6_in2,time13_in1,time13_in2;
-wire signed [32:0] plus1,plus2,plus3;
-
-// 假設 counter 從 0 數到 15 (對應 div20_1 到 div20_16)
 wire signed[31:0] so[0:5];
-wire [3:0] c_2 = counter[3:0]-1;
-
-assign  time13_in1 = (&c_2)? 0: so[0];
-assign    time13_in2 = (|c_2)?so[3]:0;
-assign    time6_in1 = (&c_2[3:1])?0:so[1];
-assign    time6_in2 = (|c_2[3:1])? so[4]:0;
-assign     plus1 = (c_2[3] & c_2[2] & (c_2[1] | c_2[0]))?0:so[2];
-assign plus2 = ((~c_2[3] & ~c_2[2]) & ~(c_2[1] & c_2[0]))?0:so[5];
-assign    plus3 = b_in_reg[c_2];
+reg signed [32:0] time6_in1,time6_in2,time13_in1,time13_in2,plus1,plus2;
+wire signed [32:0]plus3;
+wire [3:0] count = counter[3:0];
 
 
+always @* begin
+    time13_in1=so[0];
+    time13_in2=so[3];
+    time6_in1= so[1];
+    time6_in2= so[4];
+    plus1= so[2];
+    plus2= so[5];
+    case(count)
+    4'b0000: begin time13_in1 = 0; time6_in1 = 0;plus1=0; end
+    4'b0001: begin time13_in2 = 0; time6_in2 =0; plus2=0;end
+    4'b0010: begin time6_in2 =0; plus2 =0; end
+    4'b0011: plus2 =0;
+    4'b1110: plus1 =0;
+    4'b1111: begin time6_in1 = 0; plus1 =0;end
+    endcase
+end
+
+assign     plus3 = $signed({b_in_reg[count-4'b0001],16'b0});
 
 
 // 最後統一接給一個運算單元 (需包含 func_13, func_6 邏輯)
@@ -41,7 +48,7 @@ wire signed [31:0] current_x_now_val;
 // div20 single_div20_unit (.in(final_in), .out(current_x_now_val));
 wire  [65:0] b_ex = $signed(final_in);
 assign current_x_now_val =(b_ex * 28'd214748365)>>32;//除20
-ShiftRegister sr (.CLK(clk),.RESET(~reset),.shift_in(current_x_now_val),.enable(counter>0),.so1(so[0]),.so2(so[1]),.so3(so[2]),.so4(so[3]),.so5(so[4]),.so6(so[5]));                     
+ShiftRegister sr (.CLK(clk),.RESET(reset),.shift_in(current_x_now_val),.enable(counter!=0),.so1(so[0]),.so2(so[1]),.so3(so[2]),.so4(so[3]),.so5(so[4]),.so6(so[5]));                     
                                
 
 
@@ -62,7 +69,7 @@ always @(posedge clk or posedge reset) begin
     end else begin
         counter <= counter + 1;
         if (in_en) begin
-            b_in_reg[counter] <= {(b_in),16'b0}; // Example operation, replace with actual logic
+            b_in_reg[counter] <= {(b_in)}; // Example operation, replace with actual logic
         end
 
         if(counter > 16*iterations) begin 
@@ -74,6 +81,8 @@ end
 
 
 endmodule
+
+
 
 module ShiftRegister(
     input CLK,RESET, enable,
@@ -93,24 +102,20 @@ module ShiftRegister(
     assign so4=Q1[31];
     assign so5=Q1[30];
     assign so6=Q1[29];
-    always @(posedge CLK or negedge RESET) begin
-        if (~RESET)begin
+    always @(posedge CLK or posedge RESET) begin
+        if (RESET)begin
            for (i=0;i<32;i=i+1) begin
                Q1[i]<=0;
            end
            
         end
-        else begin
+        else 
+        begin
             if (enable) begin
-                for (i=0;i<32;i=i+1) begin
+                for (i=0;i<31;i=i+1) begin
                     Q1[i]<= Q1[i+1];
                 end
                 Q1[31]<= shift_in;
-            end
-            else begin
-                for (i=0;i<32;i=i+1) begin
-                    Q1[i]<= Q1[i];
-                end
             end
         end
     end      
